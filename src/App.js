1289f98273114ba3e1b633ca0e3529939f681141
Lucy <lucynn11@gmail.com>
2018-12-11 23:13:21 +0400

    setting-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from firebase;
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCiqOH1fo1rlYdVS4ZKzy_ZHX85kgMoIlU",
+  authDomain: "pomodoro-a4d1d.firebaseapp.com",
+  databaseURL: "https://pomodoro-a4d1d.firebaseio.com",
+  projectId: "pomodoro-a4d1d",
+  storageBucket: "pomodoro-a4d1d.appspot.com",
+  messagingSenderId: "108481377327"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
