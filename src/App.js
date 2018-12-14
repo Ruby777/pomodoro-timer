990a00eccd26a3a48614fcbaf25ff617953c3f25
Lucy <lucynn11@gmail.com>
2018-12-14 06:59:59 +0400

    firebase-correction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as firebase from firebase;
+import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
